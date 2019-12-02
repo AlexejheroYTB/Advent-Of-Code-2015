4adbb025f6ed1712580c5bed4265bedcc39678b9
AlexejheroYTB <alexejhero@gmail.com>
2019-12-02 10:53:31 +0200

    Update 3.cpp

--- 3/3.cpp
+++ 3/3.cpp
@@ -9,21 +9,53 @@ int main()
 	ifstream fin("data.txt");
 	char s[10000];
 	fin.get(s, 10000);
-	int x = 5000, y = 5000, t = 1;
+	int x = 5000, y = 5000, rx = 5000, ry = 5000, t = 1;
 	v[x][y]++;
-	for (unsigned int i = 0; i < strlen(s); i++) {
-		switch (s[i]) {
-		case '^':
-			y--;
-		case '>':
-			x++;
-		case 'v':
-			y++;
-		case '<':
-			x--;
+	for (int i = 0; i < (int)strlen(s); i++) {
+		if (i % 2 == 0) {
+			switch (s[i]) {
+			case '^':
+				x++;
+				//cout << "Santa moved north!\n";
+				break;
+			case '>':
+				y--;
+				//cout << "Santa moved east!\n";
+				break;
+			case 'v':
+				x--;
+				//cout << "Santa moved south!\n";
+				break;
+			case '<':
+				y++;
+				//cout << "Santa moved west!\n";
+				break;
+			}
+			v[x][y]++;
+			if (v[x][y] == 1) t++;
 		}
-		v[x][y]++;
-		if (v[x][y] == 1) t++;
+		else {
+			switch (s[i]) {
+			case '^':
+				rx++;
+				//cout << "Robo-Santa moved north!\n";
+				break;
+			case '>':
+				ry--;
+				//cout << "Robo-Santa moved east!\n";
+				break;
+			case 'v':
+				rx--;
+				//cout << "Robo-Santa moved south!\n";
+				break;
+			case '<':
+				ry++;
+				//cout << "Robo-Santa moved east!\n";
+				break;
+			}
+			v[rx][ry]++;
+			if (v[rx][ry] == 1) t++;
+		}
 	}
 	cout << t;
 	return 0;
