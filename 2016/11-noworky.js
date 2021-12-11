a2b2d3cdc74eda86b5fc2ac0e0c3ff1ce951334c
AlexejheroYTB <alexejhero@gmail.com>
2021-12-11 18:51:30 +0200

    :(

--- 2016/11-noworky.js
+++ 2016/11-noworky.js
@@ -69,10 +69,12 @@ function sortFloors(floors) {
 
 let lastState = 0, fewestSteps = 100000;
 function backtracking(floors, current, steps) {
-    if (steps > 37) return;
+    if (steps > 40) return;
 
-    if (cache.includes(getState(floors) + current)) return;
-    cache.push(getState(floors) + current);
+    if (cache.includes(getState(floors) + " " + current)) return;
+    cache.push(getState(floors) + " " + current);
+
+    if (current == 3 && floors[0].length == 0 && floors[1].length == 0 && floors[2].length == 2) debugger;
 
     sortFloors(floors);
 
@@ -97,124 +99,78 @@ function backtracking(floors, current, steps) {
     let movedTwoUp;
     if (current != 3) {
         sortFloors(floors);
-
         for (let i = 0; i < floors[current].length; i++) {
             const item1 = floors[current][i];
             for (let j = i + 1; j < floors[current].length; j++) {
                 const item2 = floors[current][j];
-
-                const state = getState(floors);
-
                 floors[current].splice(floors[current].findIndex(o => o.element == item1.element && o.type == item1.type), 1);
                 floors[current].splice(floors[current].findIndex(o => o.element == item2.element && o.type == item2.type), 1);
                 floors[current + 1].push(item1, item2);
-                let skip = false;
                 if (verify(floors[current]) && verify(floors[current + 1])) {
-                    const tempState = getState(floors);
                     movedTwoUp = true;
                     backtracking(floors, current + 1, steps + 1);
-                    const newTempState = getState(floors);
-                    if (tempState != newTempState) skip = true;
                 }
                 floors[current + 1].splice(floors[current + 1].findIndex(o => o.element == item1.element && o.type == item1.type), 1);
                 floors[current + 1].splice(floors[current + 1].findIndex(o => o.element == item2.element && o.type == item2.type), 1);
                 floors[current].push(item2, item1);
-
                 sortFloors(floors);
-
-                const newState = getState(floors);
-                if (state != newState && !skip) debugger;
             }
         }
     }
 
     // Move one item up
-    if (/*!movedTwoUp &&*/ current != 3) {
+    if (!movedTwoUp && current != 3) {
         sortFloors(floors);
-
         for (let i = 0; i < floors[current].length; i++) {
             const item = floors[current][i];
-
-            const state = getState(floors);
-
             floors[current].splice(floors[current].findIndex(o => o.element == item.element && o.type == item.type), 1);
             floors[current + 1].push(item);
-            let skip = false;
             if (verify(floors[current]) && verify(floors[current + 1])) {
-                const tempState = getState(floors);
                 backtracking(floors, current + 1, steps + 1);
-                const newTempState = getState(floors);
-                if (tempState != newTempState) skip = true;
             }
             floors[current + 1].splice(floors[current + 1].findIndex(o => o.element == item.element && o.type == item.type), 1)
             floors[current].push(item);
-
             sortFloors(floors);
-
-            const newState = getState(floors);
-            if (state != newState && !skip) debugger;
         }
     }
 
-    // Move one item down
-    let movedOneDown;
-    if (current != 0) {
-        sortFloors(floors);
-
-        for (let i = 0; i < floors[current].length; i++) {
-            const item = floors[current][i];
-
-            const state = getState(floors);
-
-            floors[current].splice(floors[current].findIndex(o => o.element == item.element && o.type == item.type), 1);
-            floors[current - 1].push(item);
-            let skip = false;
-            if (verify(floors[current]) && verify(floors[current - 1])) {
-                const tempState = getState(floors);
-                movedOneDown = true;
-                backtracking(floors, current - 1, steps + 1);
-                const newTempState = getState(floors);
-                if (tempState != newTempState) skip = true;
-            }
-            floors[current - 1].splice(floors[current - 1].findIndex(o => o.element == item.element && o.type == item.type), 1);
-            floors[current].push(item);
-
+    if (!((current == 1 && floors[current][0].length == 0) || (current == 2 && floors[current][0].length == 0 && floors[current][1].length == 0))) {
+        // Move one item down
+        let movedOneDown;
+        if (current != 0) {
             sortFloors(floors);
-
-            const newState = getState(floors);
-            if (state != newState && !skip) debugger;
-        }
-    }
-
-    // Move two items down
-    if (/*!movedOneDown &&*/ current != 0) {
-        sortFloors(floors);
-
-        for (let i = 0; i < floors[current].length; i++) {
-            const item1 = floors[current][i];
-            for (let j = i + 1; j < floors[current].length; j++) {
-                const item2 = floors[current][j];
-
-                const state = getState(floors);
-
-                floors[current].splice(floors[current].findIndex(o => o.element == item1.element && o.type == item1.type), 1);
-                floors[current].splice(floors[current].findIndex(o => o.element == item2.element && o.type == item2.type), 1);
-                floors[current - 1].push(item1, item2);
-                let skip = false;
+            for (let i = 0; i < floors[current].length; i++) {
+                const item = floors[current][i];
+                floors[current].splice(floors[current].findIndex(o => o.element == item.element && o.type == item.type), 1);
+                floors[current - 1].push(item);
                 if (verify(floors[current]) && verify(floors[current - 1])) {
-                    const tempState = getState(floors);
+                    movedOneDown = true;
                     backtracking(floors, current - 1, steps + 1);
-                    const newTempState = getState(floors);
-                    if (tempState != newTempState) skip = true;
                 }
-                floors[current - 1].splice(floors[current - 1].findIndex(o => o.element == item1.element && o.type == item1.type), 1);
-                floors[current - 1].splice(floors[current - 1].findIndex(o => o.element == item2.element && o.type == item2.type), 1);
-                floors[current].push(item1, item2);
-
+                floors[current - 1].splice(floors[current - 1].findIndex(o => o.element == item.element && o.type == item.type), 1);
+                floors[current].push(item);
                 sortFloors(floors);
+            }
+        }
 
-                const newState = getState(floors);
-                if (state != newState && !skip) debugger;
+        // Move two items down
+        if (!movedOneDown && current != 0) {
+            sortFloors(floors);
+            for (let i = 0; i < floors[current].length; i++) {
+                const item1 = floors[current][i];
+                for (let j = i + 1; j < floors[current].length; j++) {
+                    const item2 = floors[current][j];
+                    floors[current].splice(floors[current].findIndex(o => o.element == item1.element && o.type == item1.type), 1);
+                    floors[current].splice(floors[current].findIndex(o => o.element == item2.element && o.type == item2.type), 1);
+                    floors[current - 1].push(item1, item2);
+                    if (verify(floors[current]) && verify(floors[current - 1])) {
+                        backtracking(floors, current - 1, steps + 1);
+                    }
+                    floors[current - 1].splice(floors[current - 1].findIndex(o => o.element == item1.element && o.type == item1.type), 1);
+                    floors[current - 1].splice(floors[current - 1].findIndex(o => o.element == item2.element && o.type == item2.type), 1);
+                    floors[current].push(item1, item2);
+                    sortFloors(floors);
+                }
             }
         }
     }
@@ -224,3 +180,4 @@ function backtracking(floors, current, steps) {
 
 backtracking(floors, 0, 0);
 console.log("Fewest steps: " + fewestSteps);
+debugger;
